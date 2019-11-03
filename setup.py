cfbeacce35a3af1b52cfb4593987297e39bf494d
Maxim Vladimirskiy <horkhe@gmail.com>
2019-11-03 10:18:26 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -5,10 +5,6 @@ except ImportError:
     # python 3 moved md5
     from hashlib import md5
 
-with open("README.rst") as f:
-    long_description = f.read()
-
-
 tests_require = [
     "dill",
     "coverage",
@@ -18,12 +14,23 @@ tests_require = [
 ]
 
 setup(name="expiringdict",
-      version="1.1.4",
+      version="1.2.0",
       description="Dictionary with auto-expiring values for caching purposes",
-      long_description=long_description,
-      author="Anton Efimenko",
-      author_email="anton@mailgunhq.com",
-      url="https://github.com/mailgun/expiringdict",
+      long_description=open("README.rst").read(),
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.6",
+          "Topic :: Software Development :: Libraries",
+      ],
+      keywords="",
+      author="Mailgun Technologies Inc.",
+      author_email="admin@mailgun.com",
+      url="https://www.mailgun.com/",
       license="Apache 2",
       packages=find_packages(exclude=["tests"]),
       include_package_data=True,
