690dedbf5713485c81a3bc02630bb750b0fa9c52
Azide <rukuy@qq.com>
2023-10-25 19:50:43 +0800

    :recycle: 添加类型注解

--- expiringdict/__init__.py
+++ expiringdict/__init__.py
@@ -1,248 +1,175 @@
-"""
-Dictionary with auto-expiring values for caching purposes.
-
-Expiration happens on any access, object is locked during cleanup from expired
-values. Can not store more than max_len elements - the oldest will be deleted.
-
->>> ExpiringDict(max_len=100, max_age_seconds=10)
-
-The values stored in the following way:
-{
-    key1: (value1, created_time1),
-    key2: (value2, created_time2)
-}
-
-NOTE: iteration over dict and also keys() do not remove expired values!
-"""
-
-import sys
-import time
 from threading import RLock
+from datetime import datetime
+from types import MappingProxyType
+from collections.abc import Mapping
+from typing import Generic, TypeVar
 
-try:
-    from collections import OrderedDict
-except ImportError:
-    # Python < 2.7
-    from ordereddict import OrderedDict
+KT = TypeVar("KT")
+VT = TypeVar("VT")
 
 
-class ExpiringDict(OrderedDict):
-    def __init__(self, max_len, max_age_seconds, items=None):
-        # type: (Union[int, None], Union[float, None], Union[None,dict,OrderedDict,ExpiringDict]) -> None
+class ExpiringDict(Generic[KT, VT]):
+    def __init__(self, max_len: int, max_age_seconds: float, items: Mapping[KT, VT] | None = None):
+        if max_len < 1:
+            raise ValueError("max_len must be >= 1")
+        if max_age_seconds < 0:
+            raise ValueError("max_age_seconds must be >= 0")
 
-        if not self.__is_instance_of_expiring_dict(items):
-            self.__assertions(max_len, max_age_seconds)
-
-        OrderedDict.__init__(self)
         self.max_len = max_len
         self.max_age = max_age_seconds
         self.lock = RLock()
-
-        if sys.version_info >= (3, 5):
-            self._safe_keys = lambda: list(self.keys())
-        else:
-            self._safe_keys = self.keys
-
+        self._dict: dict[KT, tuple[VT, datetime]] = {}
         if items is not None:
-            if self.__is_instance_of_expiring_dict(items):
-                self.__copy_expiring_dict(max_len, max_age_seconds, items)
-            elif self.__is_instance_of_dict(items):
-                self.__copy_dict(items)
-            elif self.__is_reduced_result(items):
-                self.__copy_reduced_result(items)
+            self._build_from_mapping(items)
 
-            else:
-                raise ValueError("can not unpack items")
+    def __len__(self):
+        """Return the number of items in the dictionary."""
+        with self.lock:
+            return len([value for key, value in self._dict.items() if key in self])
 
-    def __contains__(self, key):
+    def __delitem__(self, key: KT):
+        with self.lock:
+            del self._dict[key]
+
+    def _build_from_mapping(self, items: Mapping[KT, VT]):
+        for key, value in items.items():
+            self.__setitem__(key, value)
+
+    def _is_died(self, item: tuple[VT, datetime]):
+        item_age = datetime.now() - item[1]
+        if item_age.seconds > self.max_age:
+            return True
+        return False
+
+    def __contains__(self, key: KT):
         """Return True if the dict has a key, else return False."""
         try:
             with self.lock:
-                item = OrderedDict.__getitem__(self, key)
-                if time.time() - item[1] < self.max_age:
-                    return True
-                else:
+                item = self._dict[key]
+                if self._is_died(item):
                     del self[key]
+                    return False
+                return True
         except KeyError:
-            pass
-        return False
+            return False
 
-    def __getitem__(self, key, with_age=False):
+    def __getitem__(self, key: KT):
         """Return the item of the dict.
 
         Raises a KeyError if key is not in the map.
         """
         with self.lock:
-            item = OrderedDict.__getitem__(self, key)
-            item_age = time.time() - item[1]
-            if item_age < self.max_age:
-                if with_age:
-                    return item[0], item_age
-                else:
-                    return item[0]
-            else:
+            item = self._dict[key]
+            if self._is_died(item):
                 del self[key]
                 raise KeyError(key)
+            return item[0]
 
-    def __setitem__(self, key, value, set_time=None):
-        """Set d[key] to value."""
+    def __setitem__(self, key: KT, value: VT, set_time: datetime | None = None):
+        """Set d[key] to value.
+
+        If the dictionary has no room, remove the least recently used item.
+        """
+        if not self.max_len:
+            return
+
         with self.lock:
-            if len(self) == self.max_len:
+            length = len(self)
+            if length == self.max_len and length:
                 if key in self:
                     del self[key]
                 else:
-                    try:
-                        self.popitem(last=False)
-                    except KeyError:
-                        pass
-            if set_time is None:
-                set_time = time.time()
-            OrderedDict.__setitem__(self, key, (value, set_time))
+                    self.popitem()
 
-    def pop(self, key, default=None):
+            self._dict[key] = (value, set_time or datetime.now())
+
+    def pop(self, key: KT, default: VT | None = None):
         """Get item from the dict and remove it.
 
         Return default if expired or does not exist. Never raise KeyError.
         """
         with self.lock:
             try:
-                item = OrderedDict.__getitem__(self, key)
-                del self[key]
-                return item[0]
+                value, _ = self._dict.pop(key)
+                return value
             except KeyError:
                 return default
 
-    def ttl(self, key):
+    def popitem(self):
+        """Get and remove the (key, value) pair least recently used."""
+        with self.lock:
+            return self._dict.popitem()
+
+    def ttl(self, key: KT):
         """Return TTL of the `key` (in seconds).
 
         Returns None for non-existent or expired keys.
         """
-        key_value, key_age = self.get(key, with_age=True)  # type: Any, Union[None, float]
+        _, key_age = self.get_with_age(key)
         if key_age:
             key_ttl = self.max_age - key_age
             if key_ttl > 0:
                 return key_ttl
         return None
 
-    def get(self, key, default=None, with_age=False):
+    def get(self, key: KT, default: VT | None = None):
         """Return the value for key if key is in the dictionary, else default."""
         try:
-            return self.__getitem__(key, with_age)
-        except KeyError:
-            if with_age:
-                return default, None
-            else:
+            item = self._dict[key]
+            if self._is_died(item):
+                del self[key]
                 return default
+            return item[0]
+        except KeyError:
+            return default
+
+    def get_with_age(self, key: KT, default: VT | None = None):
+        """Return the value for key if key is in the dictionary, else default."""
+        item = self._dict[key]
+        if self._is_died(item):
+            del self[key]
+            return default, None
+
+        return item[0], (datetime.now() - item[1]).seconds
 
     def items(self):
         """Return a copy of the dictionary's list of (key, value) pairs."""
-        r = []
-        for key in self._safe_keys():
-            try:
-                r.append((key, self[key]))
-            except KeyError:
-                pass
-        return r
+        return [(key, value[0]) for key, value in self._dict.items() if key in self]
 
-    def items_with_timestamp(self):
-        """Return a copy of the dictionary's list of (key, value, timestamp) triples."""
-        r = []
-        for key in self._safe_keys():
-            try:
-                r.append((key, OrderedDict.__getitem__(self, key)))
-            except KeyError:
-                pass
-        return r
+    def items_with_datetime(self):
+        """Return a copy of the dictionary's list of (key, value, datetime) triples."""
+        return [(key, value[0], value[1]) for key, value in self._dict.items() if key in self]
 
     def values(self):
         """Return a copy of the dictionary's list of values.
         See the note for dict.items()."""
-        r = []
-        for key in self._safe_keys():
-            try:
-                r.append(self[key])
-            except KeyError:
-                pass
-        return r
+        return [value[0] for key, value in self._dict.items() if key in self]
 
-    def fromkeys(self):
+    def fromkeys(self, keys: list[KT], value: VT, time: datetime | None = None):
         """Create a new dictionary with keys from seq and values set to value."""
-        raise NotImplementedError()
+        for key in keys:
+            self.__setitem__(key, value, time)
+        return self
 
     def iteritems(self):
         """Return an iterator over the dictionary's (key, value) pairs."""
-        raise NotImplementedError()
+        return ((key, value[0]) for key, value in self._dict.items() if key in self)
 
     def itervalues(self):
         """Return an iterator over the dictionary's values."""
-        raise NotImplementedError()
+        return (value[0] for key, value in self._dict.items() if key in self)
 
     def viewitems(self):
         """Return a new view of the dictionary's items ((key, value) pairs)."""
-        raise NotImplementedError()
+        return MappingProxyType({key: value[0] for key, value in self._dict.items() if key in self})
 
     def viewkeys(self):
         """Return a new view of the dictionary's keys."""
-        raise NotImplementedError()
+        return MappingProxyType({key: value[0] for key, value in self._dict.items() if key in self}).keys()
 
     def viewvalues(self):
         """Return a new view of the dictionary's values."""
-        raise NotImplementedError()
+        return MappingProxyType({key: value[0] for key, value in self._dict.items() if key in self}).values()
 
     def __reduce__(self):
-        reduced = self.__class__, (self.max_len, self.max_age, ("reduce_result", self.items_with_timestamp()))
-        return reduced
-
-    def __assertions(self, max_len, max_age_seconds):
-        self.__assert_max_len(max_len)
-        self.__assert_max_age_seconds(max_age_seconds)
-
-    @staticmethod
-    def __assert_max_len(max_len):
-        assert max_len >= 1
-
-    @staticmethod
-    def __assert_max_age_seconds(max_age_seconds):
-        assert max_age_seconds >= 0
-
-    @staticmethod
-    def __is_reduced_result(items):
-        if len(items) == 2 and items[0] == "reduce_result":
-            return True
-        return False
-
-    @staticmethod
-    def __is_instance_of_expiring_dict(items):
-        if items is not None:
-            if isinstance(items, ExpiringDict):
-                return True
-        return False
-
-    @staticmethod
-    def __is_instance_of_dict(items):
-        if isinstance(items, dict):
-            return True
-        return False
-
-    def __copy_expiring_dict(self, max_len, max_age_seconds, items):
-        # type: (Union[int, None], Union[float, None], Any) -> None
-        if max_len is not None:
-            self.__assert_max_len(max_len)
-            self.max_len = max_len
-        else:
-            self.max_len = items.max_len
-
-        if max_age_seconds is not None:
-            self.__assert_max_age_seconds(max_age_seconds)
-            self.max_age = max_age_seconds
-        else:
-            self.max_age = items.max_age
-
-        [self.__setitem__(key, value, set_time) for key, (value, set_time) in items.items_with_timestamp()]
-
-    def __copy_dict(self, items):
-        # type: (dict) -> None
-        [self.__setitem__(key, value) for key, value in items.items()]
-
-    def __copy_reduced_result(self, items):
-        [self.__setitem__(key, value, set_time) for key, (value, set_time) in items[1]]
+        return self.__class__, (self.max_len, self.max_age, self.items_with_datetime())
